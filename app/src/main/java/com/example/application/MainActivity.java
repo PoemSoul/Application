2583c7c7184b052a0d26b245352d66adb3c87578
PoemSoul <poetrysoulfly@gmail.com>
2020-01-09 13:37:51 +0800

    ToastCustom Toast样式自定义

--- app/src/main/java/com/example/application/MainActivity.java
+++ app/src/main/java/com/example/application/MainActivity.java
@@ -2,13 +2,37 @@ package com.example.application;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.application.util.ToastCustom;
 
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    private Context context = MainActivity.this;
+    private Button btnRgister;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.register_page);
+        initView();
+        initListener();
+    }
+
+    private void initView() {
+        btnRgister=(Button)findViewById(R.id.register_btn);
+    }
+
+    private void initListener() {
+        btnRgister.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        if(view.getId()==R.id.register_btn){
+            ToastCustom.getInstance(context).show("请输入用户名", 3000);
+        }
     }
 }
